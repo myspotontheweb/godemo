9ca3d1bb346c3510888588c9c271c16c3292ecd4
Mark O'Connor <mark.oconnor@teamwork.com>
2019-01-10 09:15:27 +0000

    Revised demo code

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 //
-// Example copyed from https://golang.org/doc/articles/wiki/
+// Example:  
+// https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/blob/master/hello-app/main.go
 //
 package main
 
 import (
-    "fmt"
-    "net/http"
+        "fmt"
+        "log"
+        "net/http"
+        "os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hey buddy, I love %s!", r.URL.Path[1:])
+func main() {
+        // use PORT environment variable, or default to 8080
+        port := "8080"
+        if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+                port = fromEnv
+        }
+
+        // register hello function to handle all requests
+        server := http.NewServeMux()
+        server.HandleFunc("/", hello)
+
+        // start the web server on port and accept requests
+        log.Printf("Server listening on port %s", port)
+        err := http.ListenAndServe(":"+port, server)
+        log.Fatal(err)
 }
 
-func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+        log.Printf("Serving request: %s", r.URL.Path)
+        host, _ := os.Hostname()
+        fmt.Fprintf(w, "Hello, world!\n")
+        fmt.Fprintf(w, "Version: 1.0.0\n")
+        fmt.Fprintf(w, "Hostname: %s\n", host)
 }
